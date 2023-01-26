ff509d4f86dda8f4548bc0ae512678586dd767a8
Muhammed Güner <44163870+Guner27@users.noreply.github.com>
2023-01-26 13:00:44 +0300

    Dosya Yükleme Mekanizmasını Geliştirme

--- Presentation/ECommerceAPI.API/Controllers/ProductsController.cs
+++ Presentation/ECommerceAPI.API/Controllers/ProductsController.cs
@@ -14,14 +14,17 @@ namespace ECommerceAPI.API.Controllers
     public class ProductsController : ControllerBase
     {
         readonly private IProductReadRepository _productReadRepository;
+        private readonly IWebHostEnvironment _webHostEnvironment;
         readonly private IProductWriteRepository _productWriteRepository;
 
         public ProductsController(
             IProductWriteRepository productWriteRepository,
-            IProductReadRepository productReadRepository)
+            IProductReadRepository productReadRepository,
+            IWebHostEnvironment webHostEnvironment)
         {
             _productWriteRepository = productWriteRepository;
             _productReadRepository = productReadRepository;
+            this._webHostEnvironment = webHostEnvironment;
         }
 
         #region Custom HttpClient Servisi Test kodları
@@ -84,6 +87,27 @@ namespace ECommerceAPI.API.Controllers
             await _productWriteRepository.SaveAsync();
             return Ok();
         }
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Upload()
+        {
+            //wwwroot/resource/product-images
+            string uploadPath = Path.Combine(_webHostEnvironment.WebRootPath, "resource/product-images");
+
+            if (!Directory.Exists(uploadPath))
+                Directory.CreateDirectory(uploadPath);
+
+            Random r = new();
+            foreach (IFormFile file in Request.Form.Files)
+            {
+                string fullPath = Path.Combine(uploadPath, $"{r.Next()}{Path.GetExtension(file.FileName)}");
+
+                using FileStream fileStream = new(fullPath, FileMode.Create, FileAccess.Write, FileShare.None, 1024 * 1024, useAsync: false);
+                await file.CopyToAsync(fileStream);
+                await fileStream.FlushAsync();
+            }
+            return Ok();
+        }
+
         #endregion
     }
 }
--- Presentation/ECommerceAPI.API/Program.cs
+++ Presentation/ECommerceAPI.API/Program.cs
@@ -26,6 +26,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseStaticFiles();   //wwwroot kullanabilmek için 
+
 app.UseCors();
 
 app.UseHttpsRedirection();
